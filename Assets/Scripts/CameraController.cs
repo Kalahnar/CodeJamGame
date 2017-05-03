92b90610476e0f3161414e738f293cd8daf79c23
Peter Bae <pbae@uw.edu>
2017-05-03 00:49:16 -0700

    a

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+
+    private Vector2 position;
+
+	void Start () {
+        position = this.transform.position;
+	}
+
+	void Update () {
+        if (player.transform.position.x > position.x && player.transform.position.y > position.y && player.transform.position.x < 69)
+        {
+            this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        }
+		else if (player.transform.position.x > position.x && player.transform.position.x < 69)
+        {
+            this.transform.position = new Vector3(player.transform.position.x, 0, -10);
+        }
+        else if (player.transform.position.y > position.y)
+        {
+            this.transform.position = new Vector3(0, player.transform.position.y, -10);
+        }
+    }
+}
---
+++ Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour {
+
+	public void loadStart()
+    {
+        SceneManager.LoadScene("SceneVanilla", LoadSceneMode.Single);
+    }
+
+    public void loadSettings()
+    {
+        SceneManager.LoadScene("SceneSettings", LoadSceneMode.Single);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
